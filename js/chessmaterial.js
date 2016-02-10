ac66282fc68c445bdb22fd95a4664cb2aab3abf4
Daniel Yount <factorf2@yahoo.com>
2016-02-10 01:42:54 -0600

    Create chessmaterial.js
    Added separate file for all the materials.

---
+++ js/chessmaterial.js
@@ -0,0 +1,108 @@
+			var material_black_p1 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });		
+			var material_black_p2 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 }); 		
+			var material_black_p3 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 }); 		
+			var material_black_p4 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 }); 
+			var material_black_p5 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			var material_black_p6 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 }); 
+			var material_black_p7 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 }); 
+			var material_black_p8 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			material_black_p1.name = "BP1";
+			material_black_p2.name = "BP2";
+			material_black_p3.name = "BP3"; 
+			material_black_p4.name = "BP4";
+			material_black_p5.name = "BP5"; 
+			material_black_p6.name = "BP6";
+			material_black_p7.name = "BP7";
+			material_black_p8.name = "BP8";
+			
+			var material_black_k1 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			var material_black_k2 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			material_black_k1.name = "Bk1";
+			material_black_k2.name = "Bk2";			
+			
+			var material_black_r1 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			var material_black_r2 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			material_black_r1.name = "Br1";
+			material_black_r2.name = "Br2";	
+			
+			
+			var material_black_b1 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			var material_black_b2 = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			material_black_b1.name = "Bb1";
+			material_black_b2.name = "Bb2";	
+						
+			var material_black_K = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			var material_black_Q = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 });
+			material_black_K.name = "BK";
+			material_black_Q.name = "BQ";	
+			
+			
+			var material_black = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 }); 
+			
+			var material_white_p1 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_p2 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_p3 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_p4 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_p5 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_p6 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_p7 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_p8 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			material_white_p1.name = "WP1";
+			material_white_p2.name = "WP2";
+			material_white_p3.name = "WP3"; 
+			material_white_p4.name = "WP4";
+			material_white_p5.name = "WP5"; 
+			material_white_p6.name = "WP6";
+			material_white_p7.name = "WP7";
+			material_white_p8.name = "WP8";
+				
+			
+			var material_white_k1 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_k2 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			material_white_k1.name = "Wk1";
+			material_white_k2.name = "Wk2";		
+			
+			var material_white_r1 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_r2 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			material_white_r1.name = "Wr1";
+			material_white_r2.name = "Wr2";	
+			
+			var material_white_b1 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_b2 = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			material_white_b1.name = "Wb1";
+			material_white_b2.name = "Wb2";	
+			
+			var material_white_K = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			var material_white_Q = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			material_white_K.name = "WK";
+			material_white_Q.name = "WQ";	
+			
+			var material_white = new THREE.MeshPhongMaterial( { color: 0xffffff,specular: 0xffffff,shininess: 20 } );
+			
+						
+			//var material_hold = new THREE.MeshPhongMaterial( { color: 0x000000,specular: 0xffffff,shininess: 20 } );
+				
+	
+			//setMaterial(dae, new THREE.MeshBasicMaterial({color: 0xff0000}));
+			var setMaterial = function(node, material) {
+                           node.material = material;
+                           if (node.children) {
+                           for (var i = 0; i < node.children.length; i++) {
+                           setMaterial(node.children[i], material);
+                           }
+                                              }
+                                                                   }
+	
+	
+			var setDAE = function( dae , daex , daez , daecolor ) {	
+				dae.scale.x = dae.scale.y = dae.scale.z = 0.002;
+				dae.position.x = daex;    //default 1.0
+				dae.position.y = 0.0
+				dae.position.z = daez;    // default 1.0	
+				setMaterial(dae, daecolor );
+				dae.updateMatrix();				
+				scene.add( dae );
+				}
+
+	
+				
